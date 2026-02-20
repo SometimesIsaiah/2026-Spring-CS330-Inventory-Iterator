c2c20fd7592db3a808616863ba575e5437127670
Isaiah <isaiahjbellamy@icloud.com>
2026-02-19 20:01:49 -0500

    Inventory.addItemStackNoCheck

--- src/main/java/items/Inventory.java
+++ src/main/java/items/Inventory.java
@@ -135,6 +135,7 @@ public class Inventory implements Iterable<ItemStack>, Cloneable
     public void addItemStackNoCheck(ItemStack toAdd)
     {
         // Add the missing (one) line by using `this.slots.add(????)`
+        this.slots.add(toAdd);
     }
 
     /**
