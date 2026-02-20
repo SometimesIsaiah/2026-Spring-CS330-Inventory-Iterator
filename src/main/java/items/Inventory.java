539d35cfac26e0f97f640a913801904575083967
Isaiah <isaiahjbellamy@icloud.com>
2026-02-19 20:36:51 -0500

    Inventory.toString

--- src/main/java/items/Inventory.java
+++ src/main/java/items/Inventory.java
@@ -224,7 +224,7 @@ public class Inventory implements Iterable<ItemStack>, Cloneable
 
         // Add the missing loop
         for(ItemStack stack: this.slots){
-            strBld.append(stack);
+            strBld.append(String.format(" %s%n", stack));
         }
         return strBld.toString();
     }
